0d2b101143fd6583e7334061671705e0d54db14b
udoyen <datameshprojects@gmail.com>
2020-11-01 21:31:39 +0100

    added gcloud debugger

--- server/index.js
+++ server/index.js
@@ -1,23 +1,26 @@
-require('rootpath')();
-require('@google-cloud/profiler').start();
-const express = require('express');
+require("rootpath")();
+require("@google-cloud/debug-agent").start({
+  serviceContext: { enableCanary: false },
+});
+require("@google-cloud/profiler").start();
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const bodyParser= require('body-parser');
-const errorHandler = require('_middleware/error-handler')
-app.use(bodyParser.urlencoded({ extended: false}));
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const errorHandler = require("_middleware/error-handler");
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 //api routes
-app.use('/student', require('./routes/student/studentController'));
-app.use('/teacher', require('./routes/teacher/teacherController'));
-app.use('/parent', require('./routes/parent/parentController'));
+app.use("/student", require("./routes/student/studentController"));
+app.use("/teacher", require("./routes/teacher/teacherController"));
+app.use("/parent", require("./routes/parent/parentController"));
 
 //global error handler
 app.use(errorHandler);
 
 //start server
-const port = process.env.NODE === 'production' ? (process.env.PORT || 80): 3000;
+const port = process.env.NODE === "production" ? process.env.PORT || 80 : 3000;
 
-app.listen(port, ()=> console.log("Listening  on port " + port));
+app.listen(port, () => console.log("Listening  on port " + port));
